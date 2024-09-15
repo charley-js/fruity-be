e7e555b770099a178683128fc5e0ecd82b81e142
charley-js <charleycrowson1997@gmail.com>
2024-09-15 14:00:46 +0100

    feat: add FruityController for handling http requests - implemented spin() method to handle /spin endpoint requests - utilize GameLogic processSpin() to send random generated symbols and payout calculation to SpinResult for data structure - returns result to front-end

--- fruity/src/main/java/com/fruity/fruityApp.java
+++ fruity/src/main/java/com/fruity/fruityApp.java
@@ -1,6 +1,7 @@
 package com.fruity;
 
 import io.javalin.Javalin;
+import com.fruity.controller.FruityController;
 
 public class fruityApp {
     public static void main(String[] args) {
@@ -8,7 +9,11 @@ public class fruityApp {
         Javalin app = Javalin.create().start();
 
         //create instance of fruityController to handle http requests
-        FruityController fruityController = new FruityController()
+        FruityController fruityController = new FruityController();
+
+        //Define endpoints
+        app.post("/spin", fruityController::spin );
+
 
     }
 
