e4b97414af86acac1aa5c47f70e718c6157c4920
charley-js <charleycrowson1997@gmail.com>
2024-09-15 13:06:13 +0100

    feat: initialize Javalin web server - initialized in main entry file - server start on default port

---
+++ fruity/src/main/java/com/fruity/fruityApp.java
@@ -0,0 +1,11 @@
+package com.fruity;
+
+import io.javalin.Javalin;
+
+public class fruityApp {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start();
+        
+    }
+
+}
