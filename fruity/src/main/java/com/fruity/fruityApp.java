e73e8f665ae81c6f8b184eccaed48a8456a5405f
charley-js <charleycrowson1997@gmail.com>
2024-09-15 13:21:09 +0100

    feat: add POST /spin endpoint for fruity - add POST /spin route in main entry file with Javalin - call fruityController spin() method to handle spin logic

--- fruity/src/main/java/com/fruity/fruityApp.java
+++ fruity/src/main/java/com/fruity/fruityApp.java
@@ -4,8 +4,12 @@ import io.javalin.Javalin;
 
 public class fruityApp {
     public static void main(String[] args) {
+        //initialize server
         Javalin app = Javalin.create().start();
-        
+
+        //create instance of fruityController to handle http requests
+        FruityController fruityController = new FruityController()
+
     }
 
 }
